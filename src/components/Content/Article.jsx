c26d2410cccbdbda3471e8730a27e4d967c330a0
Favixx <bogdandom52@gmail.com>
2024-02-05 23:54:40 +0200

    Update Article.jsx

--- src/components/Content/Article.jsx
+++ src/components/Content/Article.jsx
@@ -9,30 +9,32 @@ import { useTheme } from "../../contexts/ThemeContext";
 const Article = () => {
   const { theme } = useTheme();
   return (
-    <div className={`article ${theme === "dark" && "article-dark"}`}>
-      <img src={SimilarArticle} alt="Similar Article" />
+    <a href="#" aria-label="Article Link" g>
+      <div className={`article ${theme === "dark" && "article-dark"}`}>
+        <img src={SimilarArticle} alt="Similar Article" />
 
-      <h3 className="title-article">Lorem Ipsum is simply dummy</h3>
+        <h3 className="title-article">Lorem Ipsum is simply dummy</h3>
 
-      <div className="info-article">
-        <span className={`${theme === "dark" && "dark-info"}`}>
-          <FaUser
-            className={`icons-info-article ${
-              theme === "dark" && "dark-icons-info-article"
-            }`}
-          />
-          ALCO
-        </span>
-        <span className={` ${theme === "dark" && "dark-info"}`}>
-          <LuClock9
-            className={`icons-info-article ${
-              theme === "dark" && "dark-icons-stroke"
-            }`}
-          />
-          26 janvier 2023
-        </span>
+        <div className="info-article">
+          <span className={`${theme === "dark" && "dark-info"}`}>
+            <FaUser
+              className={`icons-info-article ${
+                theme === "dark" && "dark-icons-info-article"
+              }`}
+            />
+            ALCO
+          </span>
+          <span className={` ${theme === "dark" && "dark-info"}`}>
+            <LuClock9
+              className={`icons-info-article ${
+                theme === "dark" && "dark-icons-stroke"
+              }`}
+            />
+            26 janvier 2023
+          </span>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
